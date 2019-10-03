af5c6cf5a53b192d028be7bb854fddf8185ef27b
18PROGB0452 <18PROGB0452@TS-LABS-3.labs.cbtis94.local>
2019-10-03 11:37:26 -0500

    Codigo base del proyecto, se creo la calse libro y los atributos.

---
+++ JavaApplication23/src/javaapplication23/libro.java
@@ -0,0 +1,17 @@
+package javaapplication23;
+
+
+/**
+ *
+ * @author 18PROGB0452
+ */
+public class libro {
+    //atributos
+    private String titulo;
+    private String autor;
+    private int num_ejemplares;
+    private int ejemplares_prestados;
+    
+    //constructor vacio
+    p
+}
